5042f9a3577d05043feb124e604539765fa4466a
lichen <1725350830@qq.com>
2025-03-17 22:31:13 +0800

    feat: 基于gin实现http服务器

--- cmd/fb-apiserver/app/options/options.go
+++ cmd/fb-apiserver/app/options/options.go
@@ -1,17 +1,24 @@
 package options
 
 import (
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/onexstack_practice/fast_blog/internal/apiserver"
 	genericoptions "github.com/onexstack_practice/fast_blog/pkg/options"
 )
 
 type ServerOptions struct {
 	MysqlOptions *genericoptions.MysqlOptions `json:"mysql" mapstructure:"mysql"`
+	Addr         string                       `json:"addr" mapstructure:"addr"`
 }
 
 func NewServerOptions() *ServerOptions {
 	return &ServerOptions{
 		MysqlOptions: genericoptions.NewMysqlOptions(),
+		Addr:         "0.0.0.0:6666",
 	}
 }
 
@@ -21,11 +28,26 @@ func (o *ServerOptions) Validate() error {
 		return err
 	}
 
+	// 校验服务器地址
+	if o.Addr == "" {
+		return errors.New("server addr cannot be empty")
+	}
+	_, portStr, err := net.SplitHostPort(o.Addr)
+	if err != nil {
+		return fmt.Errorf("invalid server addr format: '%s': %v", o.Addr, err)
+	}
+	// 校验服务器端口是否为数字且值是否合理
+	port, err := strconv.Atoi(portStr)
+	if err != nil || port <= 0 || port > 65535 {
+		return fmt.Errorf("invalid server port: %s", portStr)
+	}
+
 	return nil
 }
 
 func (o *ServerOptions) Config() *apiserver.Config {
 	return &apiserver.Config{
 		MysqlOptions: o.MysqlOptions,
+		Addr:         o.Addr,
 	}
 }
--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -1,29 +1,57 @@
 package apiserver
 
 import (
+	"errors"
 	"log/slog"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	genericclioptions "github.com/onexstack_practice/fast_blog/pkg/options"
 )
 
 // Config存储应用配置
 type Config struct {
 	MysqlOptions *genericclioptions.MysqlOptions
+	Addr         string
 }
 
 // Server是一个服务器结构体类型
 type Server struct {
 	Config *Config
+	srv    *http.Server
 }
 
 func (cfg *Config) NewServer() *Server {
+	engine := gin.New()
+
+	// 注册404Handler
+	engine.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "404 Not Found."})
+	})
+
+	// 注册/healthz路由
+	engine.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	// 创建httpServer实例
+	httpServer := &http.Server{
+		Addr:    cfg.Addr,
+		Handler: engine,
+	}
+
 	return &Server{
 		Config: cfg,
+		srv:    httpServer,
 	}
 }
 
 func (s *Server) Run() error {
 	slog.Info("Read Mysql addr from Viper", "mysql.addr", s.Config.MysqlOptions.Addr)
+	slog.Info("Start to listen the incoming request on http address", "addr", s.Config.Addr)
 
-	select {}
+	if err := s.srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
 }
