3a3077713fcf46d54531b56799848db57aef29b6
lichen <1725350830@qq.com>
2025-03-25 17:42:10 +0800

    feat: 补充post相关的handler层实现

--- internal/apiserver/handler/post.go
+++ internal/apiserver/handler/post.go
@@ -1,18 +1,130 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"log/slog"
+
+	"github.com/gin-gonic/gin"
+	"github.com/onexstack_practice/fast_blog/internal/pkg/core"
+	"github.com/onexstack_practice/fast_blog/internal/pkg/errorx"
+	apiv1 "github.com/onexstack_practice/fast_blog/pkg/api/apiserver/v1"
+)
 
 // CreatePost 创建文章
-func (h *Handler) CreatePost(c *gin.Context) {}
+func (h *Handler) CreatePost(c *gin.Context) {
+	slog.Info("create post function call")
+
+	var rq apiv1.CreatePostRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, errorx.ErrBind, nil)
+		return
+	}
+
+	if err := h.validator.ValidateCreatePostRequest(c.Request.Context(), &rq); err != nil {
+		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
+		return
+	}
+
+	resp, err := h.biz.PostV1().Create(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+		return
+	}
+
+	core.WriteResponse(c, nil, resp)
+}
 
 // UpdatePost 更新文章
-func (h *Handler) UpdatePost(c *gin.Context) {}
+func (h *Handler) UpdatePost(c *gin.Context) {
+	slog.Info("update post function call")
+
+	var rq apiv1.UpdatePostRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, errorx.ErrBind, nil)
+		return
+	}
+
+	if err := h.validator.ValidateUpdatePostRequest(c.Request.Context(), &rq); err != nil {
+		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
+		return
+	}
+
+	resp, err := h.biz.PostV1().Update(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+		return
+	}
+
+	core.WriteResponse(c, nil, resp)
+}
 
 // DeletePost 删除文章
-func (h *Handler) DeletePost(c *gin.Context) {}
+func (h *Handler) DeletePost(c *gin.Context) {
+	slog.Info("delete post function call")
+
+	var rq apiv1.DeletePostRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, errorx.ErrBind, nil)
+		return
+	}
+
+	if err := h.validator.ValidateDeletePostRequest(c.Request.Context(), &rq); err != nil {
+		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
+		return
+	}
+
+	resp, err := h.biz.PostV1().Delete(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+		return
+	}
+
+	core.WriteResponse(c, nil, resp)
+}
 
 // GetPost 获取文章
-func (h *Handler) GetPost(c *gin.Context) {}
+func (h *Handler) GetPost(c *gin.Context) {
+	slog.Info("get post function call")
+
+	var rq apiv1.GetPostRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, errorx.ErrBind, nil)
+		return
+	}
+
+	if err := h.validator.ValidateGetPostRequest(c.Request.Context(), &rq); err != nil {
+		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
+		return
+	}
+
+	resp, err := h.biz.PostV1().Get(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+		return
+	}
+
+	core.WriteResponse(c, nil, resp)
+}
 
 // ListPost 获取文章列表
-func (h *Handler) ListPost(c *gin.Context) {}
+func (h *Handler) ListPost(c *gin.Context) {
+	slog.Info("list post function call")
+
+	var rq apiv1.ListPostRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, errorx.ErrBind, nil)
+		return
+	}
+
+	if err := h.validator.ValidateListPostRequest(c.Request.Context(), &rq); err != nil {
+		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
+		return
+	}
+
+	resp, err := h.biz.PostV1().List(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+		return
+	}
+
+	core.WriteResponse(c, nil, resp)
+}
--- internal/apiserver/pkg/validation/post.go
+++ internal/apiserver/pkg/validation/post.go
@@ -13,3 +13,15 @@ func (v *Validator) ValidateCreatePostRequest(ctx context.Context, rq *v1.Create
 func (v *Validator) ValidateUpdatePostRequest(ctx context.Context, rq *v1.UpdatePostRequest) error {
 	return nil
 }
+
+func (v *Validator) ValidateDeletePostRequest(ctx context.Context, rq *v1.DeletePostRequest) error {
+	return nil
+}
+
+func (v *Validator) ValidateGetPostRequest(ctx context.Context, rq *v1.GetPostRequest) error {
+	return nil
+}
+
+func (v *Validator) ValidateListPostRequest(ctx context.Context, rq *v1.ListPostRequest) error {
+	return nil
+}
