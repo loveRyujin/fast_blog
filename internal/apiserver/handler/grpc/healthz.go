7db033307b06d3a80fd91787b2b3b01f50e6fa77
lichen <1725350830@qq.com>
2025-06-27 23:46:20 +0800

    feat: example下新增client interceptor示例

--- internal/apiserver/handler/grpc/healthz.go
+++ internal/apiserver/handler/grpc/healthz.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"time"
 
+	"github.com/loveRyujin/fast_blog/internal/pkg/log"
 	apiv1 "github.com/loveRyujin/fast_blog/pkg/api/apiserver/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (h *Handler) Healthz(ctx context.Context, rq *emptypb.Empty) (*apiv1.HealthzResponse, error) {
+	log.With(ctx).Infow("Received health check request", "timestamp", time.Now().Format(time.DateTime))
 	return &apiv1.HealthzResponse{
 		Status:    apiv1.ServiceStatus_Healthy,
 		Timestamp: time.Now().Format(time.DateTime),
