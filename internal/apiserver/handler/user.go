02b728e7b3f2731e3eafba73d2cf877473b453c1
lichen <1725350830@qq.com>
2025-08-23 16:53:50 +0800

    refactor: Refactor handlers to use core2 request helpers
    - Replace manual request handling with core2 helper functions for all user operations
    - Use HandleJSONRequest for JSON body endpoints (Login, RefreshToken, ChangePassword, CreateUser, UpdateUser)
    - Use HandleURIRequest for URI parameter endpoints (DeleteUser, GetUser)
    - Use HandleQueryRequest for query parameter endpoints (ListUser)
    - Remove redundant request binding and validation code from all handler methods
    
    Signed-off-by: lichen <1725350830@qq.com>

--- internal/apiserver/handler/user.go
+++ internal/apiserver/handler/user.go
@@ -2,199 +2,61 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/loveRyujin/fast_blog/internal/pkg/core"
-	"github.com/loveRyujin/fast_blog/internal/pkg/errorx"
+	"github.com/loveRyujin/fast_blog/internal/pkg/core2"
 	"github.com/loveRyujin/fast_blog/internal/pkg/log"
-	v1 "github.com/loveRyujin/fast_blog/pkg/api/apiserver/v1"
 )
 
 // Login 用户登录
 func (h *Handler) Login(c *gin.Context) {
 	log.Infow("Login function called")
 
-	var request v1.LoginRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateLoginRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().Login(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleJSONRequest(c, h.biz.UserV1().Login, h.validator.ValidateLoginRequest)
 }
 
 // RefreshToken 刷新token
 func (h *Handler) RefreshToken(c *gin.Context) {
 	log.Infow("Refresh token function called")
 
-	var request v1.RefreshTokenRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateRefreshTokenRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().RefreshToken(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleJSONRequest(c, h.biz.UserV1().RefreshToken, h.validator.ValidateRefreshTokenRequest)
 }
 
 func (h *Handler) ChangePassword(c *gin.Context) {
 	log.Infow("Change user password function call")
 
-	var request v1.ChangePasswordRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateChangePasswordRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().ChangePassword(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleJSONRequest(c, h.biz.UserV1().ChangePassword, h.validator.ValidateChangePasswordRequest)
 }
 
 // CreateUser 创建用户
 func (h *Handler) CreateUser(c *gin.Context) {
 	log.Infow("Create user function call")
 
-	var request v1.CreateUserRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateCreateUserRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().Create(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleJSONRequest(c, h.biz.UserV1().Create, h.validator.ValidateCreateUserRequest)
 }
 
 // UpdateUser 更新用户信息.
 func (h *Handler) UpdateUser(c *gin.Context) {
 	log.Infow("Update user function called")
 
-	var request v1.UpdateUserRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateUpdateUserRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().Update(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleJSONRequest(c, h.biz.UserV1().Update, h.validator.ValidateUpdateUserRequest)
 }
 
 // DeleteUser 删除用户.
 func (h *Handler) DeleteUser(c *gin.Context) {
 	log.Infow("Delete user function called")
 
-	var request v1.DeleteUserRequest
-	if err := c.ShouldBindUri(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateDeleteUserRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().Delete(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleURIRequest(c, h.biz.UserV1().Delete, h.validator.ValidateDeleteUserRequest)
 }
 
 // GetUser 获取用户信息.
 func (h *Handler) GetUser(c *gin.Context) {
 	log.Infow("Get user function called")
 
-	var request v1.GetUserRequest
-	if err := c.ShouldBindUri(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateGetUserRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().Get(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleURIRequest(c, h.biz.UserV1().Get, h.validator.ValidateGetUserRequest)
 }
 
 // ListUser 获取用户列表.
 func (h *Handler) ListUser(c *gin.Context) {
 	log.Infow("List user function called")
 
-	var request v1.ListUserRequest
-	if err := c.ShouldBindQuery(&request); err != nil {
-		core.WriteResponse(c, errorx.ErrBind, nil)
-		return
-	}
-
-	if err := h.validator.ValidateListUserRequest(c.Request.Context(), &request); err != nil {
-		core.WriteResponse(c, errorx.ErrInvalidArugment.WithMessage(err.Error()), nil)
-		return
-	}
-
-	resp, err := h.biz.UserV1().List(c.Request.Context(), &request)
-	if err != nil {
-		core.WriteResponse(c, err, nil)
-		return
-	}
-
-	core.WriteResponse(c, nil, resp)
+	core2.HandleQueryRequest(c, h.biz.UserV1().List, h.validator.ValidateListUserRequest)
 }
